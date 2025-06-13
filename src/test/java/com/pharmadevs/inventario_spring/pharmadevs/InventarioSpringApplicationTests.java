7d31eec6e2de2db6dad556a9fd2a08b2bc227f0a
stevens1209 <stevens@tecnologicoismac.edu.ec>
2025-06-12 23:53:00 -0500

    deber3

---
+++ src/test/java/com/pharmadevs/inventario_spring/pharmadevs/InventarioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmadevs.inventario_spring.pharmadevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
